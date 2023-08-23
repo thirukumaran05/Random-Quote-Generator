4eb49f49dbe0440677b5533c80e82ad2537b2812
thirukumaran05 <learningthiru@gmail.com>
2023-08-23 14:36:25 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const quoteText = document.querySelector(".quote h1");
+const copy = document.getElementById("copy");
+const authorName = document.getElementById("author-name");
+let quoteArray = []
+
+async function getData(){
+    const response = await fetch('https://api.quotable.io/random?minLength=50&maxLength=100');
+    const data = await response.json();
+    quoteArray.push({
+        quote : data.content,
+        author : data.author
+    })
+        
+    quoteText.innerText = quoteArray[0].quote;
+    authorName.innerText = `~ ${quoteArray[0].author}`;
+}
+
+getData();
+
+
+copy.addEventListener("click" , function(){
+    navigator.clipboard.writeText(quoteArray[0].quote);
+    window.alert("Copied to Clipboard");
+})
